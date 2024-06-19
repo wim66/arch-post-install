0e7d0027a7ade8737841afa5add6dc72608fba12
Willem <20155866+wim66@users.noreply.github.com>
2024-06-19 10:35:46 +0200

    changes

--- arch-post-install.sh
+++ arch-post-install.sh
@@ -19,37 +19,139 @@ sleep 2
 sudo pacman -Syu --noconfirm --needed base-devel git dialog
 
 # List of packages to install via pacman without choice
-ESSENTIAL_PKGS=(
-    alsa-utils arandr archlinux-contrib ark atuin bat bluedevil breeze-grub breeze-gtk
-    breeze-plymouth chromium discover dolphin dolphin-plugins drkonqi expac eza
-    gnome-keyring gparted grub-customizer gufw gwenview intel-ucode kate kde-gtk-config
-    kdeplasma-addons kgamma kinfocenter konsole kscreen ksshaskpass ksystemlog
-    kwallet-pam kwrited lsb-release man-db man-pages nano ocean-sound-theme okular
-    openssh os-prober oxygen oxygen-sounds packagekit-qt6 pacmanlogviewer papirus-icon-theme
-    pavucontrol plasma-browser-integration plasma-desktop plasma-disks plasma-firewall
-    plasma-nm plasma-pa plasma-systemmonitor plasma-thunderbolt plasma-vault plasma-welcome
-    plasma-workspace-wallpapers powerdevil print-manager pulseaudio-equalizer-ladspa
-    sddm-kcm seahorse speedtest-cli terminus-font timeshift tlp unzip vlc wget
-    wl-clipboard xdg-desktop-portal-kde zram-generator zsh zsh-completions zsh-syntax-highlighting
-    zoxide fzf
+DEFAULT_PKGS=(
+    alsa-utils "Advanced Linux Sound Architecture utilities"
+    arandr "GUI for XRandR"
+    archlinux-contrib "Various useful Arch Linux utilities"
+    ark "Archiving tool for KDE"
+    atuin "Magical shell history"
+    bat "Cat clone with syntax highlighting"
+    bluedevil "Integrate Bluetooth with KDE workspace"
+    breeze-grub "Breeze theme for GRUB"
+    breeze-gtk "Breeze theme for GTK"
+    breeze-plymouth "Breeze theme for Plymouth"
+    chromium "Open-source web browser"
+    discover "KDE software center"
+    dolphin "File manager for KDE"
+    dolphin-plugins "Plugins for Dolphin"
+    drkonqi "Crash handler for KDE"
+    expac "Query information about installed packages"
+    eza "Modern replacement for ls"
+    gnome-keyring "Stores passwords and encryption keys"
+    gparted "GNOME Partition Editor"
+    grub-customizer "GRUB configuration editor"
+    gufw "GUI for Uncomplicated Firewall"
+    gwenview "Image viewer for KDE"
+    intel-ucode "Microcode update files for Intel CPUs"
+    kate "Advanced text editor for KDE"
+    kde-gtk-config "KDE configuration module for GTK"
+    kdeplasma-addons "Addons for KDE Plasma"
+    kgamma "Adjust display gamma settings"
+    kinfocenter "System information viewer"
+    konsole "Terminal emulator for KDE"
+    kscreen "Screen management software"
+    ksshaskpass "SSH password dialog for KDE"
+    ksystemlog "System log viewer for KDE"
+    kwallet-pam "PAM integration for KWallet"
+    kwrited "KDE daemon for writing to users"
+    lsb-release "Linux Standard Base release tool"
+    man-db "Manual page browser"
+    man-pages "Manual pages"
+    nano "Simple text editor"
+    ocean-sound-theme "Sound theme inspired by the ocean"
+    okular "Document viewer for KDE"
+    openssh "OpenSSH server and client"
+    os-prober "Utility to detect other OSes on a system"
+    oxygen "Oxygen theme"
+    oxygen-sounds "Sound theme for KDE"
+    packagekit-qt6 "Qt frontend for PackageKit"
+    pacmanlogviewer "Viewer for pacman log files"
+    papirus-icon-theme "Icon theme"
+    pavucontrol "PulseAudio Volume Control"
+    plasma-browser-integration "Browser integration for Plasma"
+    plasma-desktop "KDE Plasma desktop"
+    plasma-disks "Disk management for Plasma"
+    plasma-firewall "Firewall settings for Plasma"
+    plasma-nm "Network management for Plasma"
+    plasma-pa "Audio volume settings for Plasma"
+    plasma-systemmonitor "System monitor for Plasma"
+    plasma-thunderbolt "Thunderbolt management for Plasma"
+    plasma-vault "Encrypted vaults for Plasma"
+    plasma-welcome "Welcome screen for Plasma"
+    plasma-workspace-wallpapers "Wallpapers for Plasma workspace"
+    powerdevil "Power management for KDE"
+    print-manager "Printer management for KDE"
+    pulseaudio-equalizer-ladspa "PulseAudio equalizer module"
+    sddm-kcm "SDDM configuration module for KDE"
+    seahorse "GNOME application for managing encryption keys"
+    speedtest-cli "Command line interface for speedtest.net"
+    terminus-font "Monospace font for the console"
+    timeshift "System restore tool"
+    tlp "Advanced power management for Linux"
+    unzip "Extract compressed files in a ZIP archive"
+    vlc "Multimedia player"
+    wget "Network downloader"
+    wl-clipboard "Command-line copy/paste utilities for Wayland"
+    xdg-desktop-portal-kde "XDG desktop portal backend for KDE"
+    zram-generator "Systemd unit generator for zram swap"
+    zsh "Z shell"
+    zsh-completions "Additional completions for Zsh"
+    zsh-syntax-highlighting "Syntax highlighting for Zsh"
+    zoxide "A smarter cd command for your terminal"
+    fzf "Command-line fuzzy finder"
 )
 
 # List of packages to install via pacman with choice
 OPTIONAL_PKGS_PACMAN=(
-    linux-headers linux-lts linux-lts-headers linux-zen linux-zen-headers bleachbit chezmoi code deluge-gtk fastfetch firefox gimp inkscape
-    kbackup kcolorchooser kompare kvantum kvantum-qt5 linssid marker mc virtualbox
+    linux-headers "Kernel headers for building modules"
+    linux-lts "Long-term support Linux kernel"
+    linux-lts-headers "Headers for the LTS kernel"
+    linux-zen "Linux kernel optimized for desktop usage"
+    linux-zen-headers "Headers for the Zen kernel"
+    bleachbit "Disk cleanup utility"
+    chezmoi "Manage your dotfiles across multiple machines"
+    code "Visual Studio Code editor"
+    deluge-gtk "BitTorrent client"
+    fastfetch "Neofetch-like tool for displaying system information"
+    firefox "Web browser"
+    gimp "GNU Image Manipulation Program"
+    inkscape "Vector graphics editor"
+    kbackup "Backup tool for KDE"
+    kcolorchooser "Color picker for KDE"
+    kompare "File comparison tool"
+    kvantum "SVG-based theme engine for Qt"
+    kvantum-qt5 "Qt5 version of Kvantum"
+    linssid "Wireless network scanner"
+    marker "Markdown editor"
+    mc "Midnight Commander file manager"
+    virtualbox "Virtualization software"
 )
 
 # List of packages to install via yay with choice
 OPTIONAL_PKGS_YAY=(
-    brave-bin caffeine-ng catppuccin-gtk-theme-mocha downgrade dropbox klassy konsave
-    pamac-aur paru-bin plasma6-applets-panel-colorizer popcorntime-bin sddm-theme-catppuccin
-    tlpui-git update-grub ventoy-bin xcursor-arch-cursor-complete
+    brave-bin "Brave web browser"
+    caffeine-ng "Prevent screen from going to sleep"
+    catppuccin-gtk-theme-mocha "Catppuccin Mocha theme for GTK"
+    downgrade "Easily downgrade packages"
+    dropbox "Cloud storage"
+    klassy "A simple and modern Plasma theme"
+    konsave "Save and restore Plasma configurations"
+    pamac-aur "A Gtk3 frontend for libalpm"
+    paru-bin "AUR helper"
+    plasma6-applets-panel-colorizer "Colorize Plasma panel"
+    popcorntime-bin "Stream movies and TV shows"
+    sddm-theme-catppuccin "Catppuccin theme for SDDM"
+    tlpui-git "User interface for TLP"
+    update-grub "Update GRUB configuration"
+    ventoy-bin "Create bootable USB drive"
+    xcursor-arch-cursor-complete "Complete Arch cursor theme"
 )
 
 # List of services to enable if installed
 SERVICES=(
-    sddm tlp ufw
+    sddm # Simple Desktop Display Manager
+    tlp # Power management service
+    ufw # Uncomplicated Firewall
 )
 
 # Convert package arrays to a format suitable for dialog
@@ -57,8 +159,11 @@ generate_dialog_options() {
     local options=()
     local default_status=$1
     shift
-    for pkg in "$@"; do
-        options+=("$pkg" "$pkg" "$default_status")
+    while [ $# -gt 0 ]; do
+        local pkg=$1
+        local desc=$2
+        options+=("$pkg" "$desc" "$default_status")
+        shift 2
     done
     echo "${options[@]}"
 }
@@ -90,11 +195,10 @@ while true; do
     echo "You've selected these packages to install via yay: ${selected_optional_yay[@]}"
     echo
     read -p "Do you want to continue? (y/n): " CONFIRM
-    echo
-
     if [[ "$CONFIRM" =~ ^[Yy]$ ]]; then
         break
     elif [[ "$CONFIRM" =~ ^[Nn]$ ]]; then
+        clear
         selected_optional_pacman=$(dialog --separate-output --checklist "Select optional packages to install via pacman" 20 78 15 ${optional_pacman_options} 3>&1 1>&2 2>&3)
         clear
         dialog --msgbox "Warning: Installing packages from the AUR can take time to build. Please choose carefully which packages to install." 10 60
@@ -135,7 +239,7 @@ fi
 echo
 echo "Installing essential packages with pacman..."
 echo
-sudo pacman -S --noconfirm --needed "${ESSENTIAL_PKGS[@]}"
+sudo pacman -S --noconfirm --needed "${DEFAULT_PKGS[@]}"
 
 # Install optional packages with pacman
 if [ ${#selected_optional_pacman[@]} -ne 0 ]; then
@@ -166,7 +270,7 @@ for service in "${SERVICES[@]}"; do
 done
 
 # Set Plasma session as default if selected
-if [[ " ${ESSENTIAL_PKGS[@]} ${selected_optional_pacman[@]} " =~ " plasma-meta " ]]; then
+if [[ " ${DEFAULT_PKGS[@]} ${selected_optional_pacman[@]} " =~ " plasma-meta " ]]; then
     echo
     echo "Setting Plasma session as default..."
     echo
