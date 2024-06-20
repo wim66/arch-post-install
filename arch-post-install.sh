dd6eb33afaa5be65e93dc924626fd55e50f717fc
Willem <20155866+wim66@users.noreply.github.com>
2024-06-20 13:40:39 +0200

    changes

--- arch-post-install.sh
+++ arch-post-install.sh
@@ -103,7 +103,7 @@ DEFAULT_PKGS=(
     zsh # Z shell
     zsh-completions # Additional completions for Zsh
     zsh-syntax-highlighting # Syntax highlighting for Zsh
-    zsh-auto-suggestions 
+    zsh-autosuggestions 
     zoxide # A smarter cd command for your terminal
     fzf # Command-line fuzzy finder
 )
